9b5da7b33336d659eda2eef259eb9f61b09db0f8
Jamie Cansdale <jcansdale@github.com>
2018-10-15 15:22:31 +0100

    Add VsixTesting project

---
+++ test/TippingService.IntegrationTests/TestClass.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using Xunit;
+
+namespace Tests
+{
+    public class TestClass
+    {
+        [VsFact]
+        void FactTest()
+            => Assert.NotNull(Package.GetGlobalService(typeof(SVsWebBrowsingService)));
+
+        [VsTheory]
+        [InlineData(123)]
+        void TheoryTest(int n)
+        {
+            Assert.NotNull(Package.GetGlobalService(typeof(SVsWebBrowsingService)));
+            Assert.Equal(123, n);
+        }
+    }
+}
